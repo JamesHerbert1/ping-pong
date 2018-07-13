ff3cfb32e8ec456f7e801216c0828eb277b413c8
JamesHerbert1 <agembejames1997@gmail.com>
2018-07-13 13:49:49 +0300

    added my first scripts function

--- js/scripts.js
+++ js/scripts.js
@@ -4,7 +4,7 @@ var numbersToBePingponged = [];
 function pingPong (number){
     for (var index = 1; index <= number; index+=1){
         if(index % 15 === 0){
-            
+            numbersToBePingponged.push("pingpong");
         }
     }
 }
